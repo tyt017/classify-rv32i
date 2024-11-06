85681e212112ef5cf072def90102db173b0de494
tyt017 <cindy21320470@gmail.com>
2024-11-06 23:33:23 +0800

    modified relu.s

--- src/relu.s
+++ src/relu.s
@@ -29,7 +29,23 @@ relu:
 
 loop_start:
     # TODO: Add your own implementation
+    blt a1, x0, finish
+    lw t2, 0(a0)
+    blt t2, x0, less_than_zero
+    sw t2, 0(a0)
+    addi a1, a1, -1
+    addi a0, a0, 4
+    bge a1, x0, loop_start
+
+less_than_zero:
+    sw x0, 0(a0)
+    addi a1, a1, -1
+    addi a0, a0, 4
+    j loop_start
 
 error:
     li a0, 36          
     j exit          
+
+finish:
+    jr ra
